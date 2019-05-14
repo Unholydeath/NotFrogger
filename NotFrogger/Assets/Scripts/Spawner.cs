4ae2952455c89d24eda37483701ba419e2a46809
bdorman <bdorman@student.neumont.edu>
2019-05-13 19:47:59 -0600

    Working on spawner script

---
+++ NotFrogger/Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	[SerializeField] GameObject[] m_spawnPoint;
+
+	int m_pointIndex = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
