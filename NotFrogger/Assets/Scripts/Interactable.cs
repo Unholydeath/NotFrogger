4fb701476b5f5a3d8b2605a787cd293bb53d2ef7
bdorman <bdorman@student.neumont.edu>
2019-05-15 18:25:45 -0600

    Initial requirements met

---
+++ NotFrogger/Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	public delegate void CollisionAction();
+	public static CollisionAction OnCollision;
+
+	[SerializeField] bool m_IsHazard = false;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			if(OnCollision != null)
+			{
+				OnCollision();
+				if (!m_IsHazard) GoalReaction();
+			}
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			if (OnCollision != null)
+			{
+				OnCollision();
+				if (!m_IsHazard) GoalReaction();
+			}
+		}
+	}
+
+	void GoalReaction()
+	{
+		Debug.Log("GOOOOOOALLL!!!!!!!");
+	}
+}
--- NotFrogger/Assets/Scripts/Spawner.cs
+++ NotFrogger/Assets/Scripts/Spawner.cs
@@ -9,6 +9,25 @@ public class Spawner : MonoBehaviour
 
 	[SerializeField] GameObject m_playerRef;
 
+	private void Start()
+	{
+		Interactable.OnCollision += Respawn;
+
+		m_playerRef.transform.position = m_initPoint.position;
+		m_playerRef.transform.rotation = m_initPoint.rotation;
+	}
+
+	private void OnDestroy()
+	{
+		Interactable.OnCollision -= Respawn;
+	}
+
+	void Respawn()
+	{
+		m_playerRef.transform.position = m_rePoint.position;
+		m_playerRef.transform.rotation = m_rePoint.rotation;
+	}
+
 	public void ReMapPoint(Transform newPoint)
 	{
 		if (newPoint) m_rePoint = newPoint;
