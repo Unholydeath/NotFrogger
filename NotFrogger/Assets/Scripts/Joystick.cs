cfd69898546a52d7508309d89d5cbb5795635265
BlazingHoundoom <guernsey624@gmail.com>
2019-05-29 19:47:34 -0600

    Added actual Gyroscopic movement script and renamed gyrotest to joystick

--- NotFrogger/Assets/Scripts/Joystick.cs
+++ NotFrogger/Assets/Scripts/Joystick.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GyroSpriteMovementTest : MonoBehaviour
+public class Joystick : MonoBehaviour
 {
     public Vector3 deviceAcceleration;
     // Start is called before the first frame update
